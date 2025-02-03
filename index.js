fc30a6d0084070b847276981d66dff7ef0d8a4f4
AdityaNFS <22070347@ycce.in>
2025-02-03 19:55:29 +0530

    vercel deploy

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://code-sync-frontend-kappa.vercel.app/"],
+  origin: ["http://localhost:3000", "https://code-sync-frontend-kappa.vercel.app"],
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true,  
 }));
